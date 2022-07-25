ddfc138024a34672fedc5dd170778830cab005f8
changguo1998 <changguo1998@gmail.com>
2022-07-25 12:43:42 +0800

    support specified path

--- src/SeismicRayTrace.jl
+++ src/SeismicRayTrace.jl
@@ -4,6 +4,11 @@ MAX_STEP = 100000
 ϵ = 1.0e-5
 α = 0.1
 
+struct Phase
+    layer::Vector{Int}
+    polarization::Vector{Int}
+end
+
 """
 set!(; maxit::Int=100000, epsilon::Float64=1.0e-5, alpha::Float64=0.1)
 
@@ -57,129 +62,131 @@ function _refraction_raytrace(x0::Float64, h::AbstractVector, v::AbstractVector)
     end
     if step == MAX_STEP
         printstyled("MAX_STEP triggered, parameters:\nx0:", x0, "\nmodel_layer: ", join(string.(h), ' '),
-                    "\nmodel_vel:", join(string.(v), ' '), "\n"; color=:yellow)
+                    "\nmvel:", join(string.(v), ' '), "\n"; color=:yellow)
     end
     return p
 end
 
-"""
-raytrace(depth1, depth2, Δ, model_depth, model_velocity, method)
-
-    calculate travel time between two points in a layerd model
-
-    depth1, depth2 is depth of two points
-    Δ is horitonzal distance between these points
-    model_depth and model_velocity should be vector
-    method is a vector of `String`, in which can be any of refraction, reflection, guide
-        if you want all type of phase, it can be reduced to ["all"]
-"""
-function raytrace(dep1::Float64, dep2::Float64, Δ::Float64, model_depth::Vector{Float64},
-                  model_velocity::Vector{Float64}, method::Vector{T}) where {T<:AbstractString}
-    # check parameter
-    if min(dep1, dep2) < minimum(model_depth)
-        error("the two depth must be not less than the minimum of model_depth")
+function _layerid(h::Vector{Float64}, h0::Float64, dep::Float64)
+    c = h0
+    i = 1
+    while c <= dep
+        i += 1
+        c += h[i]
+        if i == length(h)
+            break
+        end
     end
-    if findlast(<=(dep1), model_depth) == findlast(<=(dep2), model_depth)
-        splitid = findlast(<=(dep1), model_depth)
-        tdep = [model_depth[1] - 0.5; model_depth[1:splitid]; (dep1 + dep2) / 2.0; model_depth[splitid+1:end]]
-        tvel = [0.0; model_velocity[1:splitid]; model_velocity[splitid]; model_velocity[splitid+1:end]]
+    return i
+end
+
+function _splitmodel(d1::Float64, d2::Float64, dep::Vector{Float64}, vel::VecOrMat{Float64})
+    nvel = size(vel, 2)
+    nl = length(dep)
+    l1 = findlast(<=(d1), dep)
+    l2 = findlast(<=(d2), dep)
+    if l1 == l2
+        g = vcat((dep[end] - dep[1]) * 0.1, diff([dep[1:l1]; d1; d2; dep[l1+1:end]]), 0.0)
+        u = vcat(zeros(1, nvel), vel[[1:l1; l1; l1; l1+1:nl], :])
     else
-        splitid = length(model_velocity) + 3
-        tdep = [model_depth[1] - 0.5; model_depth]
-        tvel = [0.0; model_velocity]
+        g = vcat((dep[end] - dep[1]) * 0.1, diff([dep[1:l1]; d1; dep[l1+1:l2]; d2; dep[l2+1:end]]), 0.0)
+        u = vcat(zeros(1, nvel), vel[[1:l1; l1; l1+1:l2; l2; l2+1:nl], :])
     end
+
+    nzerothick = count(iszero, g)
+    h = zeros(length(g) - nzerothick + 1)
+    v = zeros(length(g) - nzerothick + 1, nvel)
+    p = 1
+    for i = 1:length(g)-1
+        if g[i] != 0
+            h[p] = g[i]
+            v[p, :] .= u[i, :]
+            p += 1
+        end
+    end
+    h[p] = g[end]
+    v[p, :] .= u[end, :]
+
+    i1 = _layerid(h, dep[1], d1)
+    i2 = _layerid(h, dep[1], d2)
+    newlayer = zeros(Int, length(dep))
+    for i = 1:length(dep)
+        newlayer[i] = _layerid(h, dep[1], dep[i])
+    end
+    return (h, v, i1, i2, newlayer)
+end
+
+function _eqmodel(h::Vector{Float64}, v::VecOrMat{Float64}, path::Vector{Int}, phase::Vector{Int})
+    nlayer = 0
+    for i = 1:length(path)-1
+        nlayer += abs(path[i+1] - path[i])
+    end
+    l = zeros(nlayer)
+    w = zeros(nlayer)
+    p = 1
+    for i = 1:length(path)-1
+        l1 = path[i]
+        l2 = path[i+1]
+        ip = phase[i]
+        if l1 < l2
+            for j = l1:l2-1
+                l[p] = h[j]
+                w[p] = v[j, ip]
+                p += 1
+            end
+        else
+            for j = l1-1:-1:l2
+                l[p] = h[j]
+                w[p] = v[j, ip]
+                p += 1
+            end
+        end
+    end
+    return (l, w)
+end
+
+function _new_path(path::Vector{Int}, table::Vector{Int}, i1::Int, i2::Int)
+    newpath = zeros(Int, length(path) + 2)
+    newpath[1] = i1
+    newpath[end] = i2
+    for i = 1:length(path)
+        newpath[i+1] = table[path[i]]
+    end
+    return newpath
+end
+
+function _cal_time(h, v, x0)
+    p = _refraction_raytrace(x0, h, v)
+    t = _refraction_T(p, h, v)
+    xt = _refraction_X(p, h, v)
+    return (p=p, t=t, x=xt)
+end
+
+function raytrace(dep1::Float64, dep2::Float64, Δ::Float64, mdep::Vector{Float64}, mvel::VecOrMat{Float64},
+                  path::Vector{Vector{Int}}, pol::Vector{Vector{Int}}) where {T<:AbstractString}
     d1 = min(dep1, dep2)
     d2 = max(dep1, dep2)
-    i1 = findfirst(>(d1), tdep)
-    i2 = findlast(<(d2), tdep)
-    if dep1 == dep2
-        i1 = i2 = splitid + 2
+    (h, v, i1, i2, newlayer) = _splitmodel(d1, d2, mdep, mvel)
+    return map(path, pol) do pa, po
+        np = _new_path(pa, newlayer, i1, i2)
+        (l, w) = _eqmodel(h, v, np, po)
+        _cal_time(l, w, Δ)
     end
-    result = NamedTuple{(:x, :t, :p, :l, :type),Tuple{Float64,Float64,Float64,Int,String}}[]
-    if "refraction" in method || "all" in method
-        h = diff([d1; tdep[i1:i2]; d2])
-        v = tvel[i1-1:i2]
-        p0 = _refraction_raytrace(Δ, h, v)
-        if isnan(p0)
-            push!(result, (x=Δ, t=Δ / v[1], p=1 / v[1], l=0, type="refraction"))
-        else
-            push!(result,
-                  (x=_refraction_X(p0, h, v), t=_refraction_T(p0, h, v), p=p0, l=0, type="refraction"))
-        end
-    end
-    if "reflection" in method || "all" in method
-        for il = 2:length(tdep)
-            if tdep[il] == d1 || tdep[il] == d2 || il == splitid + 2
-                continue
-            end
-            if il < i1
-                h = [-diff([d1; tdep[i1-1:-1:il]]); diff([tdep[il:i2]; d2])]
-                v = [tvel[i1-1:-1:il]; tvel[il:i2]]
-            elseif il > i2
-                h = [diff([d1; tdep[i1:il]]); -diff([tdep[il:-1:i2]; d2])]
-                v = [tvel[i1-1:il-1]; tvel[il-1:-1:i2-1]]
-            else
-                continue
-            end
-            p0 = _refraction_raytrace(Δ, h, v)
-            push!(result,
-                  (x=_refraction_X(p0, h, v), t=_refraction_T(p0, h, v), p=p0,
-                   l=il > splitid + 2 ? il - 2 : il - 1,
-                   type="reflection"))
-        end
-    end
-    if "guide" in method || "all" in method
-        for il = 3:length(tdep)
-            if il == splitid + 2
-                continue
-            end
-            if il < i1
-                if tvel[il-1] < tvel[il]
-                    continue
-                end
-                p0 = 1.0 / tvel[il-1]
-                if any(>=(tvel[il-1]), tvel[il:i2])
-                    continue
-                end
-                h1 = diff([tdep[il:i1-1]; d1])
-                v1 = tvel[il:i1-1]
-                h2 = diff([tdep[il:i2-1]; d2])
-                v2 = tvel[il:i2-1]
-            elseif il > i2
-                if tvel[il-1] > tvel[il]
-                    continue
-                end
-                p0 = 1.0 / tvel[il]
-                if any(>=(tvel[il]), tvel[i1-1:il-1])
-                    continue
-                end
-                h1 = diff([d1; tdep[i1:il]])
-                v1 = tvel[i1-1:il-1]
-                h2 = diff([d2; tdep[i2:il]])
-                v2 = tvel[i2-1:il-1]
-            else
-                continue
-            end
-            x1 = _refraction_X(p0, h1, v1)
-            x2 = _refraction_X(p0, h2, v2)
-            if Δ < (x1 + x2)
-                continue
-            end
-            t1 = _refraction_T(p0, h1, v1)
-            t2 = _refraction_T(p0, h2, v2)
-            t3 = (Δ - x1 - x2) * p0
-            push!(result, (x=Δ, t=t1 + t2 + t3, p=p0, l=il > splitid + 2 ? il - 2 : il - 1, type="guide"))
-        end
-    end
-    return result
 end
 
-function raytrace(dep1::Real, dep2::Real, Δ::Real, model_depth::Vector{<:Real},
-                  model_velocity::Vector{<:Real}, method::Union{AbstractString,Vector{<:AbstractString}})
-    if typeof(method) <: AbstractString
-        method = [method]
-    end
-    return raytrace(Float64(dep1), Float64(dep2), Float64(Δ), Float64.(model_depth),
-                    Float64.(model_velocity), String.(method))
+function raytrace(dep1::Float64, dep2::Float64, Δ::Float64, mdep::Vector{Float64}, mvel::VecOrMat{Float64},
+                  path::Vector{Int}, pol::Vector{Int}) where {T<:AbstractString}
+    d1 = min(dep1, dep2)
+    d2 = max(dep1, dep2)
+    (h, v, i1, i2, newlayer) = _splitmodel(d1, d2, mdep, mvel)
+    np = _new_path(path, newlayer, i1, i2)
+    (l, w) = _eqmodel(h, v, np, pol)
+    return _cal_time(l, w, Δ)
 end
+
+function raytrace(dep1::Real, dep2::Real, Δ::Real, mdep::Vector{<:Real}, mvel::VecOrMat{<:Real},
+                  path::Vector{<:Integer}, pol::Vector{<:Integer})
+    return raytrace(Float64(dep1), Float64(dep2), Float64(Δ), Float64.(mdep), Float64.(mvel), Int.(path), Int.(pol))
+end
+
 end
